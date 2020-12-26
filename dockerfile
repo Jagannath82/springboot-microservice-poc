b64c22f96b674444530db89233b78fd9dd04dfd0
Delta_Techops_ATL <techopsline@Delta-Techops-ATLs-MacBook-Pro.local>
2020-12-26 02:39:54 -0500

    docker compose

--- dockerfile
+++ dockerfile
@@ -1,4 +1,7 @@
 FROM openjdk:8-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
+VOLUME /tmp
+ARG JAR_FILE
+COPY ./build/libs/springboot-microservice-poc-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/myclinic/doctor/registration/config/MongoDBConfig.java
+++ src/main/java/com/myclinic/doctor/registration/config/MongoDBConfig.java
@@ -13,7 +13,7 @@ public class MongoDBConfig {
    * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
    */
    public @Bean MongoClient mongoClient() {
-       return MongoClients.create("mongodb://localhost:27017");
+       return MongoClients.create("mongodb://api-database:27017");
    }
    
    public @Bean MongoTemplate mongoTemplate() {
